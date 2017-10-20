92ce97a67db33c5a80d03e441e6cc21bcc5dddbf
gh_feng <583399013@qq.com>
2017-10-20 18:04:08 +0800

    Update TestGit.java

--- src/TestGit.java
+++ src/TestGit.java
@@ -5,6 +5,6 @@
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("client shu chu 111");
+        System.out.println("client shu chu 111 服务器加个 2");
     }
 }
